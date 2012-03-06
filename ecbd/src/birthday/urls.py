148b70dd8d165e9619bcbffc99fc482e167eef27
david.cifuentes <david.cifuentes@eforcers.com>
2012-03-06 16:50:41 -0500

    Cambios en la lógica de recorrer los namespaces para el envío de correo

--- ecbd/src/birthday/urls.py
+++ ecbd/src/birthday/urls.py
@@ -1,8 +1,4 @@
 from django.conf.urls.defaults import patterns, url
-import birthday.tasks
-
-#for signals to work
-
 
 urlpatterns = patterns('birthday.views',
     url(r'^$', 'welcome', name='landing'),
@@ -10,7 +6,5 @@ urlpatterns = patterns('birthday.views',
             
     url(r'^admin/client/setup/$', 'client_setup', name='client_setup'), 
     url(r'^admin/client/clear_cache/$', 'clear_cache', name='clear_cache'), 
-    
-    
     
 )
--- ecbd/src/birthday/views.py
+++ ecbd/src/birthday/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from birthday.decorators import admin_login_required
-from birthday.models import Client, User
 from birthday.forms import make_client_form
+from birthday.helpers import ProfilesHelper
+from birthday.models import Client, User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.signals import user_logged_out
 from django.core.urlresolvers import reverse
@@ -9,13 +10,19 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from google.appengine.api import namespace_manager
+from google.appengine.ext import deferred
 from google.appengine.ext.db.metadata import get_namespaces
 from oauth.views import ACCESS_TOKEN
 import datetime
 import gdata.gauth
 import logging
 import re
-from birthday.helpers import ProfilesHelper
+import settings
+
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+
+ECBD_SENDER_EMAIL = getattr(settings, 'ECBD_SENDER_EMAIL')
 
 def sign_out(request, domain):
     """
@@ -158,17 +165,36 @@ def sync_with_profile(request):
                     logging.debug("Birthday data for [%s] day [%s] month [%s] year [%s]", 
                                  username, birth_day, birth_month, birth_year)
                     user = User.objects.get_or_create(email= "%s@%s" % (username,client.domain))[0]
-                    #TODO: save only when something have changed
-                    user.birth_day = birth_day
-                    user.birth_month = birth_month
-                    user.birth_year = birth_year
-                    user.save()
+                    #Save only if something have changed
+                    needs_to_save = False
+                    if user.birth_day != birth_day:
+                        user.birth_day = birth_day
+                        needs_to_save = True
+                    if user.birth_month != birth_month:
+                        user.birth_month = birth_month
+                        needs_to_save = True
+                    if user.birth_year != birth_year:
+                        user.birth_year = birth_year
+                        needs_to_save = True
+                    if needs_to_save:
+                        user.save()
 
     #Restore to the original namespace
     namespace_manager.set_namespace(current_namespace)
     return HttpResponse(content="Birthday dates were sync from profiles successfully", status=200)
 
-def send_birthday_messages(request):
+def send_birthday_message(celebrant_pk):
+    celebrant = User.objects.get(pk=celebrant_pk)
+    client = Client.objects.get_from_cache()
+    #TODO: Bring dinamically the HTML content from a Google Site instead of a static template
+    body = render_to_string('birthday_card.html',
+                            {'celebrant': celebrant})
+    #TODO: Define the sender email dinamically
+    send_mail(client.subject, body, ECBD_SENDER_EMAIL,
+              [celebrant.email])
+    
+
+def send_daily_birthday_messages(request):
     """
     It goes through all the namespaces making queries to the datastore for 
     users who have the same birth day and birth month as today
@@ -178,13 +204,14 @@ def send_birthday_messages(request):
     current_namespace = namespace_manager.get_namespace()
     for namespace in get_namespaces():
         # Forget about the default empty namespace
-        logging.debug("Checking celebrants for today day %s month %s namespace %s", today.month, today.day, namespace.namespace_name)
-        if namespace and namespace.namespace_name:
-            namespace_manager.set_namespace(namespace.namespace_name)
+        if namespace:
+            logging.debug("Checking celebrants for today day %s month %s namespace %s", today.month, today.day, namespace)
+            namespace_manager.set_namespace(namespace)
             celebrants = User.objects.filter(receive_mail=True, birth_month=today.month, birth_day=today.day)
             for celebrant in celebrants:
                 logging.debug("Found a celebrant for today! %s", celebrant)
-            #TODO: schedule sending email
+                #Schedule sending email
+                deferred.defer(send_birthday_message, celebrant.pk, _queue="mail-queue")
     #Restore to the original namespace
     namespace_manager.set_namespace(current_namespace)
     return HttpResponse(content="Birthday messages were scheduled", status=200)
